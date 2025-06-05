14b3863f230a60d373e9032af9c1528670edc2ea
ZEERO\slodo <slodoban2006@gmail.com>
2025-06-05 17:27:51 +0300

    Add comments

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import random
 
-random_number = random.randint(1, 100)
+random_number = random.randint(1, 100) # Generating random number
 print('Вітаю, я загадав число від 1 до 100. Спробуй відгадати його за 7 спроб')
-attempt = 7
+attempt = 7 # Attempt quantity
 
+# Verification of input
 
 while attempt > 0 :
 
-    valid = 0
+    valid = 0 # verification flag
 
     while valid == 0:
 
@@ -16,16 +17,17 @@ while attempt > 0 :
 
         try:
 
-            num_of_user = int(user_input)
+            num_of_user = int(user_input) #verification of int input
             valid = 1
 
-            if num_of_user > 100 or num_of_user < 1:
+            if num_of_user > 100 or num_of_user < 1: #verefication of range
                 print("Число повинно бути в межах від 1 до 100")
                 valid = 0
 
         except ValueError:
             print("Invalid input")
 
+# Checking game rules
 
     if num_of_user < random_number:
 
